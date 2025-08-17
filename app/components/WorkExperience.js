bac96fe2c17a073e8a3cda869f639fea68d86b5e
Daniel-Lidman <dlidman@gmail.com>
2025-08-17 11:40:18 -0500

    Bug fix to get deployed to Vercel

--- app/components/WorkExperience.js
+++ app/components/WorkExperience.js
@@ -6,21 +6,23 @@ function WorkExperience() {
       company: 'Ramsey Solutions',
       title: 'Senior Video Editor',
       dates: 'Oct 2023 - Present',
-      headline:
-        '',
-      bulletPoints: ['Produced 50+ high-quality videos annually for a YouTube audience of 5M+, strengthening engagement and brand growth.', 'Collaborated with cross-functional teams to translate business goals into digital experiences.', 'Streamlined editing workflows while maintaining quality, demonstrating optimization and problem-solving skills.'],
+      headline: '',
+      bulletPoints: [
+        'Produced 50+ high-quality videos annually for a YouTube audience of 5M+, strengthening engagement and brand growth.',
+        'Collaborated with cross-functional teams to translate business goals into digital experiences.',
+        'Streamlined editing workflows while maintaining quality, demonstrating optimization and problem-solving skills.',
+      ],
     },
     {
       company: 'Love Has No Limits',
       title: 'Video Editor',
       dates: 'Jan 2016 - Oct 2023',
-      headline:
-        '',
+      headline: '',
       bulletPoints: [
         'Directed the video department, overseeing 5+ interns and contractors, and led a $50,000 custom studio build-out,—demonstrating technical leadership and project ownership.',
         'Produced 400+ videos for 9 international and domestic humanitarian campaigns, reaching stadium audiences of 60,000+ and millions online.',
         'Delivered high-profile content for The Freedom Experience featuring Justin Bieber & Chance the Rapper, managing high-pressure deadlines and complex production pipelines.',
-        'Partnered with executive leadership to shape messaging and digital storytelling.'
+        'Partnered with executive leadership to shape messaging and digital storytelling.',
       ],
     },
     {
@@ -29,9 +31,10 @@ function WorkExperience() {
       dates: '2018 - 2025',
       headline:
         'Along with my full-time position I’ve also taken on freelance video editing work. I have had the great opportunity to work with amazing brands and grow my skills alongside great creative minds.',
-        bulletPoints: ['Delivered video content for diverse brands and agencies, managing projects from requirements to final delivery.', 
-        'Adapted to new workflows and tools, reinforcing a fast learning curve now applied to JavaScript and web.'
-]
+      bulletPoints: [
+        'Delivered video content for diverse brands and agencies, managing projects from requirements to final delivery.',
+        'Adapted to new workflows and tools, reinforcing a fast learning curve now applied to JavaScript and web.',
+      ],
     },
   ];
 
@@ -48,15 +51,14 @@ function WorkExperience() {
 export default WorkExperience;
 
 function Job({ job }) {
-
   return (
     <div className="pb-4">
       <p className="uppercase inline font-bold text-sm">{job.title} | </p>
       <p className="inline text-xs font-semibold text-blue-900">{job.dates}</p>
       <p className="text-xs italic">{job.company}</p>
-      <ul className='list-disc text-xs ml-3'>
+      {/* <ul className='list-disc text-xs ml-3'>
         {job.bulletPoints.map((bullet)=> (<li>{bullet}</li>))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
